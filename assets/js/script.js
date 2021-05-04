b10dca11c0e427dfef763fc9fc6241ddf052e78d
MiguelConQueso <michael_pnkstn@yahoo.com>
2021-05-04 18:05:01 -0400

    created sections and added js

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function myFunction1() {
+    var x = document.getElementById("projects");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction2() {
+    var x = document.getElementById("contact");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction3() {
+    var x = document.getElementById("resume");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction4() {
+    var x = document.getElementById("more");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
